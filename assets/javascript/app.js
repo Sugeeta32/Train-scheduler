14a1ab796f16837ad887d7bfd1518b31a6507d7b
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-10-10 11:35:41 -0400

    Html skeleton and forms

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// alert("working");
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCGzUc14DzYQu2fAoAoqEV7XkphtSkywi4",
+    authDomain: "train-3469e.firebaseapp.com",
+    databaseURL: "https://train-3469e.firebaseio.com",
+    projectId: "train-3469e",
+    storageBucket: "train-3469e.appspot.com",
+    messagingSenderId: "172388692365",
+    appId: "1:172388692365:web:c4c1126e22f3f0daa30dc4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
