dfa022bbabc9ed3b904a8b84714afa68b2f8ec67
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-10-12 22:55:16 -0400

    Firebase working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,127 @@
-// alert("working");
-// Your web app's Firebase configuration
+
+
+
+alert("what");
+console.log("what");
+//your code here
+
 var firebaseConfig = {
-    apiKey: "AIzaSyCGzUc14DzYQu2fAoAoqEV7XkphtSkywi4",
-    authDomain: "train-3469e.firebaseapp.com",
-    databaseURL: "https://train-3469e.firebaseio.com",
-    projectId: "train-3469e",
-    storageBucket: "train-3469e.appspot.com",
-    messagingSenderId: "172388692365",
-    appId: "1:172388692365:web:c4c1126e22f3f0daa30dc4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyCBOq5Yl7GMqCE6M1GPwzjp-7-u3LdTfdU",
+    authDomain: "scheduler-cb850.firebaseapp.com",
+    databaseURL: "https://scheduler-cb850.firebaseio.com",
+    projectId: "scheduler-cb850",
+    storageBucket: "scheduler-cb850.appspot.com",
+    messagingSenderId: "474869784644",
+    appId: "1:474869784644:web:584a6ccb5a6748ba625c11"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var startTime = "";
+var frequency = 0;
+
+function currentTime() {
+    var current = moment().format("LT");
+    $("#currentTime").html(current);
+    setTimeout(currentTime, 1000);
+};
+
+
+$(".form-field").on("keyup",function(){
+var traintemp=$("#train-name").val().trim();
+var citytemp=$("#destination").val().trim();
+var timetemp=$("#first-train").val().trim();
+var freqtemp=$("#frequency").val().trim();
+
+sessionStorage.setItem("train",traintemp);
+sessionStorage.setItem("city",citytemp);
+sessionStorage.setItem("time",timetemp);
+sessionStorage.setItem("freq",freqtemp);
+});
+$("#train-name").val(sessionStorage.getItem("train"));
+$("#destination").val(sessionStorage.getItem("city"));
+$("#first-train").val(sessionStorage.getItem("time"));
+$("#frequency").val(sessionStorage.getItem("freq"));
+
+
+$("#submit").on("click", function (event) {
+    // Don't refresh the page!
+    event.preventDefault();
+// if($("#train-name").val().trim()===""||
+// $("#destination").val().trim()===""||
+// $("#first-train").val().trim()===""||
+// $("#frequency").val().trim()===""){
+//     alert("please fill");
+// }else{
+
+
+
+
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = $("#first-train").val().trim();
+    frequency = $("#frequency").val().trim();
+
+$(".form-field").val("");
+    // Code in the logic for storing and retrieving the most recent user.
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        frequency: frequency,
+        startTime: startTime,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+    // Don't forget to provide initial data to your Firebase database.
+    sessionStorage.clear();
+//  }
+
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+
+    //     //moment.js
+    var firstTrainNew = moment(childSnapshot.val().startTime, "hh:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+    var remaining = diffTime % childSnapshot.val().frequency;
+    //minutes for next train
+    var minAway = childSnapshot.val().frequency - remaining;
+    //next train time
+    var nextTrain = moment().add(minAway, "minutes");
+    var key = childSnapshot.key;
+
+
+
+
+    var newrow = $("<tr>");
+
+    newrow.append($("<td>" + childSnapshot.val().trainName + "</td>"));
+    newrow.append($("<td>" + childSnapshot.val().destination + "</td>"));
+    newrow.append($("<td >" + childSnapshot.val().frequency + "</td>"));
+    newrow.append($("<td>" + moment(nextTrain).format("LT") + "</td>"));
+    newrow.append($("<td>" + minAway + "</td>"));
+    newrow.append($("<td><button class='arrival btn btn-danger btn-xs' data-key='" + key + "'>X</button></td>"));
+
+
+    if (minAway < 8) {
+        newrow.addClass("info");
+    }
+    $("#train-table-rows").append(newrow);
+
+});
+
+$(document).on("click", ".arrival", function () {
+    keyref = $(this).attr("data-key");
+    database.ref().child(keyref).remove();
+    window.location.reload();
+});
+
+currentTime();
+
+
+//  function (errorObject) {
+
+//     console.log("Errors handled" + errorObject.code)
+// };
